aba4882cd6cb691baeaba7b81b797ed9d6229abe
Jakob Löw <jakob@m4gnus.de>
2019-02-17 00:19:21 +0100

    telegram_bot.py: convert to tabs

--- telegram_bot.py
+++ telegram_bot.py
@@ -12,32 +12,32 @@ updater = Updater(config["telegram_token"])
 currentlyBroken = []
 
 def sendFailures(broken):
-    global currentlyBroken
+	global currentlyBroken
 
-    if currentlyBroken == broken:
-        return
-    currentlyBroken = broken
+	if currentlyBroken == broken:
+		return
+	currentlyBroken = broken
 
-    file = genAnimation(broken)
-    with open(file, "r") as fd:
-        updater.bot.send_animation(config["group_id"], fd)
+	file = genAnimation(broken)
+	with open(file, "r") as fd:
+		updater.bot.send_animation(config["group_id"], fd)
 
 def genAndRespond(update, broken):
-    file = genAnimation(broken)
-    with open(file, "r") as fd:
-        res = update.message.reply_animation(fd)
+	file = genAnimation(broken)
+	with open(file, "r") as fd:
+		res = update.message.reply_animation(fd)
 
 def status(bot, update):
-    genAndRespond(update, currentlyBroken)
+	genAndRespond(update, currentlyBroken)
 
 def demo(bot, update):
-    broken_bitset = random.randrange(1, 255)
-    broken = []
-    for i in range(0, 8):
-        if broken_bitset & (1 << i):
-            broken.append(i + 1)
+	broken_bitset = random.randrange(1, 255)
+	broken = []
+	for i in range(0, 8):
+		if broken_bitset & (1 << i):
+			broken.append(i + 1)
 
-    genAndRespond(update, broken)
+	genAndRespond(update, broken)
 
 startListener(config, sendFailures)
 
